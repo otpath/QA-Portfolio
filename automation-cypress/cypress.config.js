ec104814ea9dedffa41a809190cb454ac149d2ce
otpath <patriciaotarao@gmail.com>
2026-01-23 03:40:07 -0300

    Fix Cypress configuration to disable support file

--- automation-cypress/cypress.config.js
+++ automation-cypress/cypress.config.js
@@ -1,9 +1,13 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-});
+    supportFile: false,
+    specPattern: 'cypress/e2e/**/*.cy.js'
+  }
+})
+
+
+
+
+
